5d825655c7f990159e12d5ccd8969890e0bbc7c7
Ebrahim Sameh <ebrahimsameh16@gmail.com>
2022-05-19 19:43:15 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,6 +34,9 @@ const IndexPage = ({ data }) => {
             <button onClick={()=>{navigate("/products")}} className="btn-ui mt-4 min-w-full lg:min-w-0">
               Check our products
             </button>
+            <button onClick={()=>{navigate("/products")}} className="btn-ui mt-4 min-w-full lg:min-w-0">
+              Check our products
+            </button>
           </div>
         </div>
       </div>
